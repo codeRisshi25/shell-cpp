ee1539cefe3f947807001949ded32b8a352a0395
Risshi25 <risshirajsen@gmail.com>
2025-06-03 00:52:41 +0530

    codecrafters submit [skip ci]

--- src/tokenize.cpp
+++ src/tokenize.cpp
@@ -3,12 +3,13 @@
 #include <iostream>
 #include <cctype>
 
-// Finite State Machine 
-enum class ParseMode {
+// Finite State Machine
+enum class ParseMode
+{
     NORMAL,
     IN_SINGLE_QUOTE,
     IN_DOUBLE_QUOTE,
-    IN_ESCAPE
+    IN_ESCAPE,
 };
 
 // custom tokenizer for quoting
@@ -17,43 +18,98 @@ std::vector<std::string> tokenize(const std::string &input)
     std::string token;
     std::vector<std::string> tokens;
     ParseMode mode = ParseMode::NORMAL;
+    ParseMode prevMode = ParseMode::NORMAL;
 
     // looping through the arguments for tokens
-    for (char i : input ){
-        switch (mode) {
-            case ParseMode:: NORMAL :
-            // case for normal 
-            if (i==' '){if (!token.empty()) tokens.push_back(token); token.clear(); }
-            else if (i == '\'') mode = ParseMode::IN_SINGLE_QUOTE;
-            else if (i == '\"') mode = ParseMode::IN_DOUBLE_QUOTE;
-            else if (i == '\\') mode = ParseMode::IN_ESCAPE;
-            else {token.push_back(i);};
-
+    for (char i : input)
+    { 
+        switch (mode)
+        {
+        case ParseMode::NORMAL:
+            // case for normal
+            if (i == ' ')
+            {
+                if (!token.empty())
+                    tokens.push_back(token);
+                token.clear();
+            }
+            else if (i == '\'')
+                mode = ParseMode::IN_SINGLE_QUOTE;
+            else if (i == '\"')
+                mode = ParseMode::IN_DOUBLE_QUOTE;
+            else if (i == '\\')
+            {
+                mode = ParseMode::IN_ESCAPE;
+                prevMode = ParseMode::NORMAL;
+            }
+            else
+            {
+                token.push_back(i);
+            }
             break;
 
-
-            case  ParseMode:: IN_SINGLE_QUOTE:
+        case ParseMode::IN_SINGLE_QUOTE:
             // case for single quotes
-            if (i=='\'') mode = ParseMode::NORMAL;
-            else if (i == '\\') mode = ParseMode::IN_ESCAPE;
-            else token.push_back(i);
+            if (i == '\'')
+                mode = ParseMode::NORMAL;
+            else
+                token.push_back(i);
             break;
 
-            case ParseMode:: IN_DOUBLE_QUOTE :
-            // case for double quotes 
-            if (i=='\"') mode = ParseMode::NORMAL;
-            else token.push_back(i);
+        case ParseMode::IN_DOUBLE_QUOTE:
+            // case for double quotes
+            if (i == '\"')
+                mode = ParseMode::NORMAL;
+            else if (i == '\\')
+            {
+                mode = ParseMode::IN_ESCAPE;
+                prevMode = ParseMode::IN_DOUBLE_QUOTE;
+            }
+            else
+                token.push_back(i);
             break;
 
-            case ParseMode:: IN_ESCAPE :
-            if (i=='\\') token.push_back(' ');
-            else {
-             mode = ParseMode::NORMAL;
-             token.push_back(i);
-            }    
+        case ParseMode::IN_ESCAPE:
+            if (prevMode == ParseMode::IN_DOUBLE_QUOTE)
+            {
+                if (i == '\\' || i == '$' || i == '\"') 
+                {
+                    token.push_back(i);
+                }
+                else {
+                    token.push_back('\\');
+                    token.push_back(i);
+                }
+                mode = prevMode; // Reset to previous mode
+            }
+            else 
+            {
+                // if (i == 'n')
+                //     token.push_back('\n');
+                // else if (i == 't')
+                //     token.push_back('\t');
+                if (i == '\\')
+                    token.push_back(' ');
+                else
+                {
+                    token.push_back(i);
+                }
+                mode = prevMode; // Reset to previous mode
+            }
+            break;
         }
     }
-    if (!token.empty()) tokens.push_back(token);
-    if (mode != ParseMode::NORMAL) { std::cout << "invalid command\n" ; tokens = {}; };
+    
+    // Add any remaining token
+    if (!token.empty())
+        tokens.push_back(token);
+    
+    // Check for unclosed quotes or incomplete escape sequences
+    if (mode != ParseMode::NORMAL)
+    {
+        std::cout << "invalid command\n";
+        return {};
+    }
+    
     return tokens;
-};
+}
